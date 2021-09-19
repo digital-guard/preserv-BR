983826480a6811f8f319cd0cfffe5b4f2e9fea98
cchness <cness@riseup.net>
2021-09-19 11:08:44 -0300

    Trata a injestão de layer com multiplos arquivos. Cria versão inicial de template para SP. Cria yaml exemplo para injestão de edificios e lotes de SP.

---
+++ src/maketemplates/make_ref035a.mustache.mk
@@ -0,0 +1,179 @@
+##
+## Template file reference: preserv-BR/data/SP/SaoPaulo/_pk035
+## tplId: 035a
+##
+tplInputSchema_id=035a
+
+
+## BASIC CONFIG
+pg_io  ={{pg_io}}
+orig   ={{orig}}
+pg_uri ={{pg_uri}}
+sandbox_root={{sandbox}}
+need_commands= 7z v16+; psql v12+; shp2pgsql v3+; {{need_extra_commands}}
+srid   ={{srid}}
+
+pkid = {{pkid}}
+fullPkID={{pkid}}_{{pkversion}}
+sandbox=$(sandbox_root)/_pk$(fullPkID)
+
+## USER CONFIGS
+pg_db  ={{pg_db}}
+thisTplFile_root = {{thisTplFile_root}}
+
+## ## ## ## ## ## ##
+## THIS_MAKE, _pk{{pkid}}
+
+{{#files}}
+part{{p}}_file  ={{file}}
+part{{p}}_name  ={{name}}
+{{/files}}
+
+## COMPOSED VARS
+pg_uri_db   =$(pg_uri)/$(pg_db)
+{{#files}}
+part{{p}}_path  =$(orig)/$(part{{p}}_file)
+{{/files}}
+
+all:
+	@echo "=== Resumo deste makefile de recuperação de dados preservados ==="
+	@printf "Targets para a geração de layers:\n\tall_layers {{#layers_keys}}{{.}} {{/layers_keys}}\n"
+	@printf "Demais targets implementados:\n\tclean wget_files me\n"
+	@echo "A gereação de layers requer os seguintes comandos e versões:\n\t$(need_commands)"
+
+all_layers: {{#layers_keys}}{{.}} {{/layers_keys}}
+	@echo "--ALL LAYERS--"
+
+## ## ## ## ## ## ## ## ##
+## ## ## ## ## ## ## ## ##
+## Make targets of the Project Digital Preservation
+{{#layers}}
+
+{{#geoaddress}}## ## ## ## sponsored by Project AddressForAll
+geoaddress: layername = geoaddress_{{subtype}}
+geoaddress: tabname = pk$(fullPkID)_p{{file}}_geoaddress
+geoaddress: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "geoaddress" datatype (point with house_number but no via name)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*" ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+geoaddress-clean: tabname = pk$(fullPkID)_p{{file}}_geoaddress
+geoaddress-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE"
+{{/geoaddress}}
+
+{{#building}}## ## ## ## sponsored by Project AddressForAll
+building: layername = building_{{subtype}}
+building: tabname = pk$(fullPkID)_p{{file}}_building
+building: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "building" datatype (point with house_number but no via name)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*" ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql_multiplefiles_zipped}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+building-clean: tabname = pk$(fullPkID)_p{{file}}_building
+building-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE"
+{{/building}}
+
+{{#nsvia}}## ## ## ## sponsored by Project AddressForAll
+nsvia: layername = nsvia_{{subtype}}
+nsvia: tabname = pk$(fullPkID)_p{{file}}_nsvia
+nsvia: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "nsvia" datatype (zone with name)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*"  ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+nsvia-clean: tabname = pk$(fullPkID)_p{{file}}_nsvia
+nsvia-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE;  DROP VIEW IF EXISTS vw_$(tabname) CASCADE;"
+{{/nsvia}}
+
+{{#via}}## ## ## ## sponsored by Project AddressForAll
+via: layername = via_{{subtype}}
+via: tabname = pk$(fullPkID)_p{{file}}_via
+via: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "via" datatype (street axes)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*"  ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+via-clean: tabname = pk$(fullPkID)_p{{file}}_via
+via-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE"
+{{/via}}
+
+{{#parcel}}## ## ## ## sponsored by Project AddressForAll
+parcel: layername = parcel_{{subtype}}
+parcel: tabname = pk$(fullPkID)_p{{file}}_parcel
+parcel: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "parcel" datatype (street axes)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*" ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql_multiplefiles_zipped}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+parcel-clean: tabname = pk$(fullPkID)_p{{file}}_parcel
+parcel-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE"
+{{/parcel}}
+
+{{#block}}## ## ## ## sponsored by Project AddressForAll
+block: layername = block_{{subtype}}
+block: tabname = pk$(fullPkID)_p{{file}}_block
+block: makedirs $(part{{file}}_path)
+	@# pk{{pkid}}_p{{file}} - ETL extrating to PostgreSQL/PostGIS the "block" datatype (street axes)
+{{>common002_layerHeader}}
+	cd $(sandbox);  7z {{7z_opts}} x -y  $(part{{file}}_path) "{{orig_filename}}*" ; chmod -R a+rx . > /dev/null
+{{>common003_shp2pgsql}}
+{{>common001_pgAny_load}}
+	@echo FIM.
+
+block-clean: tabname = pk$(fullPkID)_p{{file}}_block
+block-clean:
+	rm -f "$(sandbox)/{{orig_filename}}.*" || true
+	psql $(pg_uri_db) -c "DROP TABLE IF EXISTS $(tabname) CASCADE"
+{{/block}}
+
+{{/layers}}
+## ## ## ## ## ## ## ## ##
+## ## ## ## ## ## ## ## ##
+
+makedirs: clean_sandbox
+	@mkdir -p $(sandbox_root)
+	@mkdir -p $(sandbox)
+	@mkdir -p $(pg_io)
+
+wget_files:
+	@echo "Under construction, need to check that orig path is not /var/www! or use orig=x [ENTER if not else ^C]"
+	@echo $(orig)
+	@read _ENTER_OK_
+	mkdir -p $(orig)
+{{#files}}
+	@cd $(orig); wget http://preserv.addressforall.org/download/{{file}}
+{{/files}}
+	@echo "Please, if orig not default, run 'make _target_ orig=$(orig)'"
+
+
+## ## ## ##
+
+clean_sandbox:
+	@rm -rf $(sandbox) || true
+
+clean: geoaddress-clean nsvia-clean via-clean
