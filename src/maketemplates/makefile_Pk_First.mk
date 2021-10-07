adf28fbc05779c2880f1f0a1ab6456361c2dc390
cchness <cness@riseup.net>
2021-10-07 16:57:53 -0300

    Possibilita que seja gerado esqueleto de readme.md de um pacote de dados com make readme. Versão inicial.

--- src/maketemplates/makefile_Pk_First.mk
+++ src/maketemplates/makefile_Pk_First.mk
@@ -14,6 +14,22 @@ thisTplFile       = $(thisTplFile_root)/src/maketemplates/make_$(schemaId_templa
 mkme_srcTpl       =  $(baseSrc)/$(thisTplFile)
 mkme_output       = /tmp/digitalPresservation-make_me.mk
 
+readme_srcTpl     =  $(baseSrc)/$(thisTplFile_root)/src/maketemplates/readme.mustache
+readme_output     = /tmp/README_me.md
+
+readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
+	@echo "-- Updating this make --"
+	python3 $(srcPy) -b $(baseSrc)/ -t $(readme_srcTpl) -i $(mkme_input)  > $(readme_output)
+	@echo " Check diff, the '<' lines are the new ones... Something changed?"
+	@diff $(readme_output) ./README.md || :
+	@echo "If some changes, and no error in the changes, move the script:"
+	@echo " mv ./README.md ./bk-README.md"
+	@echo " mv $(readme_output) ./README.md"
+	@echo "[ENTER para rodar mv ou ^C para sair]"
+	@read _tudo_bem_
+	mv $(readme_output) ./README.md
+
+
 me: $(srcPy) $(mkme_input0) $(mkme_input) $(mkme_srcTpl)
 	@echo "-- Updating this make --"
 	python3 $(srcPy) -b $(baseSrc)/ -t $(mkme_srcTpl) --tplLast=$(mkme_srcTplLast) -i $(mkme_input) --input0=$(mkme_input0) > $(mkme_output)
