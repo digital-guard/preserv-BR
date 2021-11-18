5c8d43512ab2026744db578b764170a459895cff
cchness <cness@riseup.net>
2021-11-18 14:54:47 -0300

    Associa jurisdição e pkid aos nomes de arquivos temporários.

--- src/maketemplates/commomLast.mustache.mk
+++ src/maketemplates/commomLast.mustache.mk
@@ -12,14 +12,15 @@ mkme_input0       = $(baseSrc)/preserv-$(country)/src/maketemplates/commomFirst.
 mkme_srcTplLast   = $(baseSrc)/preserv-$(country)/src/maketemplates/commomLast.mustache.mk
 
 thisTplFile_root  = $(shell grep 'thisTplFile_root'  < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+sandbox           = $(shell grep 'sandbox'           < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
 schemaId_template = $(shell grep 'schemaId_template' < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-thisTplFile       = $(thisTplFile_root)/src/maketemplates/make_$(schemaId_template).mustache.mk
+pkid              = $(shell grep 'pkid'              < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
 
-mkme_srcTpl       = $(baseSrc)/$(thisTplFile)
-mkme_output       = /tmp/digitalPresservation-make_me.mk
+mkme_srcTpl       = $(baseSrc)/$(thisTplFile_root)/src/maketemplates/make_$(schemaId_template).mustache.mk
+mkme_output       = $(sandbox)/make_me.mk_$(country)_pkid$(pkid)
 
 readme_srcTpl     = $(baseSrc)/preserv-$(country)/src/maketemplates/readme.mustache
-readme_output     = /tmp/README-draft.md
+readme_output     = $(sandbox)/README-draft.md_$(country)_pkid$(pkid)
 
 script_quotes     = $(baseSrc)/preserv/src/quotes.bash
 
@@ -39,7 +40,6 @@ readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
 	@echo "[ENTER para rodar mv ou ^C para sair]"
 	@read _tudo_bem_
 	mv $(readme_output) ./README-draft.md
-	chmod 777 ./README-draft.md
 
 me: $(srcPy) $(mkme_input0) $(mkme_input) $(mkme_srcTpl) $(script_quotes)
 	@echo "-- Updating this make --"
@@ -54,4 +54,3 @@ me: $(srcPy) $(mkme_input0) $(mkme_input) $(mkme_srcTpl) $(script_quotes)
 	@echo "[ENTER para rodar mv ou ^C para sair]"
 	@read _tudo_bem_
 	mv $(mkme_output) ./makefile
-	chmod 777 ./makefile
