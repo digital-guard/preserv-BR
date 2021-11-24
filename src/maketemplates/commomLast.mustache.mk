4e6e01e64600542c328c45b6bae2cc98197791e9
cchness <cness@riseup.net>
2021-11-24 16:12:23 -0300

    Prepara make me para rodar o mustache por meio do banco de dados.

--- src/maketemplates/commomLast.mustache.mk
+++ src/maketemplates/commomLast.mustache.mk
@@ -3,26 +3,31 @@
 ## SELF-GENERATE MAKE (make me)
 ##
 
-mkme_input        = ./make_conf.yaml
+mkme_input        = $(shell ls -d "${PWD}/"make_conf.yaml)
 country           = BR
+#country           = $(shell ls -d "${PWD}" | cut -d'-' -f2 | cut -d'/' -f1)
 baseSrc           = /opt/gits/_dg
 
 srcPy             = $(baseSrc)/preserv/src/run_mustache.py
 mkme_input0       = $(baseSrc)/preserv-$(country)/src/maketemplates/commomFirst.yaml
 mkme_srcTplLast   = $(baseSrc)/preserv-$(country)/src/maketemplates/commomLast.mustache.mk
 
-thisTplFile_root  = $(shell grep 'thisTplFile_root'  < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-sandbox           = $(shell grep 'sandbox'           < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-schemaId_template = $(shell grep 'schemaId_template' < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-pkid              = $(shell grep 'pkid'              < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+thisTplFile_root  = $(shell grep 'thisTplFile_root'  < $(mkme_input0) | cut -f2 -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_io             = $(shell grep 'pg_io'             < $(mkme_input0) | cut -f2 -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+schemaId_template = $(shell grep 'schemaId_template' < $(mkme_input)  | cut -f2 -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pkid              = $(shell grep 'pkid'              < $(mkme_input)  | cut -f2 -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
 
 mkme_srcTpl       = $(baseSrc)/$(thisTplFile_root)/src/maketemplates/make_$(schemaId_template).mustache.mk
-mkme_output       = $(sandbox)/make_me.mk_$(country)_pkid$(pkid)
+mkme_output       = $(pg_io)/make_me.mk_$(country)_pkid$(pkid)
 
 readme_srcTpl     = $(baseSrc)/preserv-$(country)/src/maketemplates/readme.mustache
-readme_output     = $(sandbox)/README-draft.md_$(country)_pkid$(pkid)
+readme_output     = $(pg_io)/README-draft.md_$(country)_pkid$(pkid)
 
 script_quotes     = $(baseSrc)/preserv/src/quotes.bash
+
+pg_uri            = $(shell grep 'pg_uri' < $(mkme_input0) | cut -f2- -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_db             = $(shell grep 'pg_db'  < $(mkme_input0) | cut -f2  -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_uri_db         = $(pg_uri)/$(pg_db)
 
 info:
 	@echo "=== Targets  ==="
@@ -31,6 +36,7 @@ info:
 
 readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
 	@echo "-- Create basic readme.md template --"
+# 	psql $(pg_uri_db) -c "SELECT ingest.generate_makefile('$(baseSrc)','$(readme_srcTpl)','$(mkme_input)')" > $(readme_output)
 	python3 $(srcPy) -b $(baseSrc)/ -t $(readme_srcTpl) -i $(mkme_input)  > $(readme_output)
 	chmod 777 $(readme_output)
 	@echo " Check diff, the '<' lines are the new ones... Something changed?"
@@ -43,6 +49,7 @@ readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
 
 me: $(srcPy) $(mkme_input0) $(mkme_input) $(mkme_srcTpl) $(script_quotes)
 	@echo "-- Updating this make --"
+# 	psql $(pg_uri_db) -c "SELECT ingest.generate_makefile('$(baseSrc)','$(mkme_srcTpl)','$(mkme_srcTplLast)','$(mkme_input)','$(mkme_input0)')" > $(mkme_output)
 	python3 $(srcPy) -b $(baseSrc)/ -t $(mkme_srcTpl) --tplLast=$(mkme_srcTplLast) -i $(mkme_input) --input0=$(mkme_input0) > $(mkme_output)
 	chmod 777 $(mkme_output)
 	bash $(script_quotes) $(mkme_output)
