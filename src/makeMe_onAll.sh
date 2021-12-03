ddd6ff75bf1f33a3767da088b74b66d6d96c789b
cchness <cness@riseup.net>
2021-12-03 12:57:35 -0300

    Movido commomLast para preserv. Simplifica o makefile.

--- src/makeMe_onAll.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# retorna todos os diretórios com o padrão '*_pk*' e copia o arquivo makefile_Pk_First para cada um deles, renomeando o arquivo para makefile.
-find ../data -type d -name '*_pk*' -exec cp maketemplates/commomLast.mustache.mk \{\}/makefile \;
